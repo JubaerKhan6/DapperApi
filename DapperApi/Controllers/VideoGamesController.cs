804e067304cc10cb37d80e4fc17811f2d5bf4e5a
Zubair <zubairfaheem6@gmail.com>
2022-10-30 10:12:12 +0600

    Add project files.

---
+++ DapperApi/Controllers/VideoGamesController.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace DapperApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGamesController : ControllerBase
+    {
+        private readonly SqlConnection connection;
+
+        public VideoGamesController(IConfiguration config)
+        {
+            connection = new SqlConnection(config.GetConnectionString("DefaultConnection"));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<VideoGames>>> GetAllGames()
+        {
+            var games = await connection.QueryAsync<VideoGames>("select * from VideoGames");
+
+            return Ok(games);
+        }
+        [HttpGet("(int:id)")]
+
+        public async Task<ActionResult<List<VideoGames>>> GetGame(int gameid)
+        {
+            var game = await connection.QueryAsync<VideoGames>("select * from VideoGames where id=@Id",
+            new { Id = gameid });
+            return Ok(game);
+        }
+
+       
+
+    }
+}
---
+++ DapperApi/VideoGames.cs
@@ -0,0 +1,15 @@
+﻿namespace DapperApi
+{
+    public class VideoGames
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+
+        public int size { get; set; }
+
+        public string Developer { get; set; } = string.Empty;
+
+        public int Released { get; set; }
+    }
+}
